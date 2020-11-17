84fc282cd1d09f1b030cfad57f2dc3f85a7cf28d
singhvikram1692 <singhvikram1692@gmail.com>
2020-11-18 01:17:01 +0530

    duplicateZeros

--- src/arrays/DuplicateZeros.java
+++ src/arrays/DuplicateZeros.java
@@ -2,6 +2,24 @@ package arrays;
 
 public class DuplicateZeros {
 
+
+/*
+    Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+    Note that elements beyond the length of the original array are not written.
+
+    Do the above modifications to the input array in place, do not return anything from your function.
+
+    Input: [1,0,2,3,0,4,5,0]
+    Output: null
+    Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+
+    Input: [1,2,3]
+    Output: null
+    Explanation: After calling your function, the input array is modified to: [1,2,3]
+*/
+
     public void duplicateZeros(int[] arr) {
         for(int i =0; i< arr.length-1;i++){
             if(arr[i] == 0){
